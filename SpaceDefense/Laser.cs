215379d66146f4071a08218d68808628c2d9e14d
Tarik Onalan <phi.quantum@gmail.com>
2014-06-24 16:10:17 -0700

    Working on Lasers and Levels

--- SpaceDefense/Laser.cs
+++ SpaceDefense/Laser.cs
@@ -10,13 +10,29 @@ namespace SpaceDefense
 {
     class Laser : GameObject
     {
+        public static float SPEED = 16;
+
         protected Vector3f velocity;
         protected int zTarget;
 
         public Laser(Vector3f position, GameObject target)
             : base("LASER", 2, 32, "laser.png")
         {
+            zTarget = target.ZOrder;
+            velocity = new Vector3f(new Vector3f(target.Position.X - position.X, target.Position.Y - position.Y, target.ZOrder).Normalize(), SPEED);
+            Rotation = new Vector3f(0, 1, 0).Angle(velocity);
+        }
 
+        public override void Update()
+        {
+            base.Update();
+
+            Position.X -= velocity.X;
+            Position.Y -= velocity.Y;
+            ZOrder -= (int)velocity.Z;
+
+            Scale.X = 10f / ZOrder;
+            Scale.Y = 10f / ZOrder;
         }
     }
 }
