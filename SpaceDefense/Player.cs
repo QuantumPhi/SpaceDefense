f01d01948e0389076ba9ca455afcab6ad4b20e0d
Tarik Onalan <phi.quantum@gmail.com>
2014-06-27 15:18:48 -0700

    Working on drag with ugly ternary operators

--- SpaceDefense/Player.cs
+++ SpaceDefense/Player.cs
@@ -13,7 +13,7 @@ namespace SpaceDefense
         public Vector2f velocity;
         public XboxController XBController { get; private set; }
 
-        public Player() : base("PLAYER", 64, 64, "ship.png") 
+        public Player() : base("PLAYER", 70, 64, "ship.png") 
         {
             velocity = new Vector2f();
             XBController = new XboxController(SlimDX.XInput.UserIndex.Two);
@@ -29,12 +29,26 @@ namespace SpaceDefense
 
             if (XBController.LeftStick.Position.X > 0.01f || XBController.LeftStick.Position.X < -0.01f)
             {
-                Position.X = Math.Abs(Position.X + XBController.LeftStick.Position.X * 10) < Game.WindowWidth / 2 ? Position.X + XBController.LeftStick.Position.X * 10 : XBController.LeftStick.Position.X < 0 ? -Game.WindowWidth / 2 : Game.WindowWidth / 2;
+                velocity.X = XBController.LeftStick.Position.X * 10;
+                Position.X = Math.Abs(Position.X + velocity.X) < Game.WindowWidth / 2 ? Position.X + velocity.X : XBController.LeftStick.Position.X < 0 ? -Game.WindowWidth / 2 : Game.WindowWidth / 2;
+            }
+            else
+            {
+                velocity.X = velocity.X < 0 ? velocity.X + 0.25f > 0 ? 0 : velocity.X + 0.25f : velocity.X - 0.25f < 0 ? 0 : velocity.X - 0.25f;
+                Position.X = Math.Abs(Position.X + Math.Cos(Rotation * Math.PI / 180 + 90) * velocity.X) < Game.WindowWidth / 2 ? (float)(Position.X + Math.Cos(Rotation * Math.PI / 180 + 90) * velocity.X) : XBController.LeftStick.Position.X < 0 ? -Game.WindowWidth / 2 : Game.WindowWidth / 2;
+
             }
             if (XBController.LeftStick.Position.Y > 0.01f || XBController.LeftStick.Position.Y < -0.01f)
             {
-                Position.Y = Math.Abs(Position.Y + XBController.LeftStick.Position.Y * 10) < Game.WindowHeight / 2 ? Position.Y + XBController.LeftStick.Position.Y * 10 : XBController.LeftStick.Position.Y < 0 ? -Game.WindowHeight / 2 : Game.WindowHeight / 2;
+                velocity.Y = XBController.LeftStick.Position.Y * 10;
+                Position.Y = Math.Abs(Position.Y + velocity.Y) < Game.WindowHeight / 2 ? Position.Y + velocity.Y : XBController.LeftStick.Position.Y < 0 ? -Game.WindowHeight / 2 : Game.WindowHeight / 2;
             }
+            else
+            {
+                velocity.Y = velocity.Y < 0 ? velocity.Y + 0.25f > 0 ? 0 : velocity.Y + 0.25f : velocity.Y - 0.25f < 0 ? 0 : velocity.Y - 0.25f;
+                Position.Y = Math.Abs(Position.Y + Math.Sin(Rotation * Math.PI / 180 + 90) * velocity.Y) < Game.WindowHeight / 2 ? (float)(Position.Y + Math.Sin(Rotation * Math.PI / 180 + 90) * velocity.Y) : XBController.LeftStick.Position.Y < 0 ? -Game.WindowHeight / 2 : Game.WindowHeight / 2;
+            }
+            
         }
     }
 }
