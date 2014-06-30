3cee012b192a6626d276d5517427afc9cc004e53
Tarik Onalan <phi.quantum@gmail.com>
2014-06-30 09:58:58 -0700

    Removed drag

--- SpaceDefense/Player.cs
+++ SpaceDefense/Player.cs
@@ -32,23 +32,11 @@ namespace SpaceDefense
                 velocity.X = XBController.LeftStick.Position.X * 10;
                 Position.X = Math.Abs(Position.X + velocity.X) < Game.WindowWidth / 2 ? Position.X + velocity.X : XBController.LeftStick.Position.X < 0 ? -Game.WindowWidth / 2 : Game.WindowWidth / 2;
             }
-            else
-            {
-                velocity.X = velocity.X < 0 ? velocity.X + 0.25f > 0 ? 0 : velocity.X + 0.25f : velocity.X - 0.25f < 0 ? 0 : velocity.X - 0.25f;
-                Position.X = Math.Abs(Position.X + Math.Cos(Rotation * Math.PI / 180 + 90) * velocity.X) < Game.WindowWidth / 2 ? (float)(Position.X + Math.Cos(Rotation * Math.PI / 180 + 90) * velocity.X) : XBController.LeftStick.Position.X < 0 ? -Game.WindowWidth / 2 : Game.WindowWidth / 2;
-
-            }
             if (XBController.LeftStick.Position.Y > 0.01f || XBController.LeftStick.Position.Y < -0.01f)
             {
                 velocity.Y = XBController.LeftStick.Position.Y * 10;
                 Position.Y = Math.Abs(Position.Y + velocity.Y) < Game.WindowHeight / 2 ? Position.Y + velocity.Y : XBController.LeftStick.Position.Y < 0 ? -Game.WindowHeight / 2 : Game.WindowHeight / 2;
             }
-            else
-            {
-                velocity.Y = velocity.Y < 0 ? velocity.Y + 0.25f > 0 ? 0 : velocity.Y + 0.25f : velocity.Y - 0.25f < 0 ? 0 : velocity.Y - 0.25f;
-                Position.Y = Math.Abs(Position.Y + Math.Sin(Rotation * Math.PI / 180 + 90) * velocity.Y) < Game.WindowHeight / 2 ? (float)(Position.Y + Math.Sin(Rotation * Math.PI / 180 + 90) * velocity.Y) : XBController.LeftStick.Position.Y < 0 ? -Game.WindowHeight / 2 : Game.WindowHeight / 2;
-            }
-            
         }
     }
 }
