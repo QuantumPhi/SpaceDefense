e36609afb96237c5c1b9471d961a7f5f7e41dbdc
Tarik Onalan <phi.quantum@gmail.com>
2014-06-30 12:25:16 -0700

    Bullets and shooting

---
+++ SpaceDefense/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using Engine;
+using SpaceDefense.Engine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceDefense
+{
+    class Bullet : GameObject
+    {
+        public static float SPEED = 20;
+
+        protected Vector2f velocity;
+
+        public Bullet(Vector2f direction)
+            : base("BULLET", 4, 8, "bullet.png")
+        {
+            velocity = new Vector2f(direction.Normalize() as Vector2f, SPEED);
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            Position.X -= velocity.X;
+            Position.Y -= velocity.Y;
+
+            if (Position.X > Game.WindowWidth / 2 + 100 || Position.X < -Game.WindowWidth / 2 - 100 ||
+                Position.Y > Game.WindowHeight / 2 + 100 || Position.Y < -Game.WindowHeight / 2 - 100)
+                IsDead = true;
+        }
+    }
+}
--- SpaceDefense/Player.cs
+++ SpaceDefense/Player.cs
@@ -11,6 +11,7 @@ namespace SpaceDefense
     class Player : GameObject
     {
         public Vector2f velocity;
+        public float rot;
         public XboxController XBController { get; private set; }
 
         public Player() : base("PLAYER", 70, 64, "ship.png") 
@@ -25,7 +26,10 @@ namespace SpaceDefense
 
             XBController.Update();
 
-            Rotation = XBController.LeftStick.Position.X == 0 && XBController.LeftStick.Position.Y == 0 ? Rotation : (float)(Math.Atan2(XBController.LeftStick.Position.Y, XBController.LeftStick.Position.X) * 180 / Math.PI) - 90;
+            rot = XBController.LeftStick.Position.X == 0 && XBController.LeftStick.Position.Y == 0 ? rot : (float)(Math.Atan2(XBController.LeftStick.Position.Y, XBController.LeftStick.Position.X) * 180 / Math.PI) - 90;
+            rot = XBController.RightStick.Position.X == 0 && XBController.RightStick.Position.Y == 0 ? rot : (float)(Math.Atan2(XBController.RightStick.Position.Y, XBController.RightStick.Position.X) * 180 / Math.PI) - 90;
+
+            Rotation += AngleDifference(Rotation, rot) / 5;
 
             if (XBController.LeftStick.Position.X > 0.01f || XBController.LeftStick.Position.X < -0.01f)
             {
@@ -37,6 +41,24 @@ namespace SpaceDefense
                 velocity.Y = XBController.LeftStick.Position.Y * 10;
                 Position.Y = Math.Abs(Position.Y + velocity.Y) < Game.WindowHeight / 2 ? Position.Y + velocity.Y : XBController.LeftStick.Position.Y < 0 ? -Game.WindowHeight / 2 : Game.WindowHeight / 2;
             }
+
+            if (XBController.RightStick.Position.X > 0.01f || XBController.RightStick.Position.X < -0.01f || XBController.RightStick.Position.Y > 0.01f || XBController.RightStick.Position.Y < -0.01f)
+            {
+                Vector2f vel = new Vector2f(-XBController.RightStick.Position.X, -XBController.RightStick.Position.Y);
+                GameObject bullet = new Bullet(vel);
+                bullet.Position = Position;
+                bullet.ZOrder = -1;
+                double angle = Math.Atan2(vel.Y, vel.X) + Math.PI / 2;
+                bullet.Rotation = (float)(angle * 180 / Math.PI);
+                bullet.Position.X += (float)(24 * Math.Cos(angle + Math.PI / 2));
+                bullet.Position.Y += (float)(24 * Math.Sin(angle + Math.PI / 2));
+                ObjectManager.AddGameObject(bullet);
+            }
+        }
+
+        public float AngleDifference(float angle1, float angle2)
+        {
+            return -(((((angle1 - angle2) % 360) + 540) % 360) - 180);
         }
     }
 }
